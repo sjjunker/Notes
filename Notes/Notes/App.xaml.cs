9f0027c361f11bca27fc4d0b834d1029d1cc1645
sjjunker <sandi.junker@outlook.com>
2025-03-24 18:16:11 -0500

    Switch to Mac

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Notes.Services;
-using Notes.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace Notes
 {
@@ -12,8 +8,6 @@ namespace Notes
         public App()
         {
             InitializeComponent();
-
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
--- Notes/Notes/Views/AboutPage.xaml.cs
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.ComponentModel;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Notes.Views
 {
@@ -10,6 +9,12 @@ namespace Notes.Views
         public AboutPage()
         {
             InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
         }
     }
 }
